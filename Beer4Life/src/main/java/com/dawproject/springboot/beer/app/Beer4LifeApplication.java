3f54643904a0f491bae02191fb58c57ef1c6a380
Pablo Pinto <pabloctender@gmail.com>
2019-05-23 08:46:22 +0200

    First Push , Project Base

---
+++ Beer4Life/src/main/java/com/dawproject/springboot/beer/app/Beer4LifeApplication.java
@@ -0,0 +1,13 @@
+package com.dawproject.springboot.beer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beer4LifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beer4LifeApplication.class, args);
+	}
+
+}
---
+++ Beer4Life/src/test/java/com/dawproject/springboot/beer/app/Beer4LifeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dawproject.springboot.beer.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Beer4LifeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
