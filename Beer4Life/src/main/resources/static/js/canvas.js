31da8e64b11c581d263ad157c05c430b3067e171
Pablo Pinto <pabloctender@gmail.com>
2019-06-11 13:58:07 +0200

    html changes

--- Beer4Life/src/main/resources/static/js/canvas.js
+++ Beer4Life/src/main/resources/static/js/canvas.js
@@ -20,7 +20,7 @@ function drawFace(ctx, radius) {
   ctx.fill();
   grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
   grad.addColorStop(0, '#333');
-  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(0.5, 'orange');
   grad.addColorStop(1, '#333');
   ctx.strokeStyle = grad;
   ctx.lineWidth = radius*0.1;
